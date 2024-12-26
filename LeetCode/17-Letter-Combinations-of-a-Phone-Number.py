d4739ac65bb6f0efa3692f3a0a0c2d49fa0d1294
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2024-12-26 19:29:40 +0900

    [17] [Time Beats: 100.00%] [Memory Beats: 6.62%] - LeetPush

--- LeetCode/17-Letter-Combinations-of-a-Phone-Number.py
+++ LeetCode/17-Letter-Combinations-of-a-Phone-Number.py
@@ -1,27 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def dfs(index, path):
+        letter_map = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+
+        output = []
+        def backtrack(path):
             if len(path) == len(digits):
-                result.append(path)
+                if len(path) > 0:
+                    output.append(path)
                 return
 
-            for i in range(index, len(digits)):
-                for j in dic[digits[i]]:
-                    dfs(i + 1, path + j)
+            for item in letter_map[digits[len(path)]]:
+                backtrack(path + item)
 
-        if not digits:
-            return []
-
-        dic = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"}
-        result = []
-        dfs(0, "")
-
-        return result
+        backtrack('')
+        return output

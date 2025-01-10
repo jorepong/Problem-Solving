d1f64b467c59813aab4ba0424805a8a854728d15
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-01-10 17:17:39 +0900

    [783] [Time Beats: 11.38%] [Memory Beats: 27.06%] - LeetPush

---
+++ LeetCode/783-Minimum-Distance-Between-BST-Nodes.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    prev = -sys.maxsize
+    result = sys.maxsize
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return
+        
+        self.minDiffInBST(root.left)
+        self.result = min(self.result, root.val - self.prev)
+        self.prev = root.val
+        self.minDiffInBST(root.right)
+
+        return self.result

bc47751288bcab24feeff0b423cbc65e90356526
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-01-27 18:44:15 +0900

    [179] [Time Beats: 50.93%] [Memory Beats: 23.59%] - LeetPush

---
+++ LeetCode/179-Largest-Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        def compare(n1, n2):
+            if str(n1) + str(n2) > str(n2) + str(n1):
+                return -1
+            else:
+                return 1
+
+        nums = sorted(nums, key=cmp_to_key(compare))
+
+        return str(int(''.join(map(str, nums))))

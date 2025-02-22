abe78284bfbcafa14169fc66c4016364d479f109
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-02-22 16:38:52 +0900

    [76] [Time Beats: 73.91%] [Memory Beats: 55.05%] - LeetPush

--- LeetCode/76-Minimum-Window-Substring.py
+++ LeetCode/76-Minimum-Window-Substring.py
@@ -1,30 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_map = {}
-        for c in t:
-            t_map[c] = 1 + t_map.get(c, 0)
+        t_map = collections.Counter(t)
+
+        need = len(t)
+
+        left = 0
+        min_len = float('inf')
+        min_context = [-1, -1]
+        for right in range(len(s)):
+            c = s[right]
+
+            need -= t_map[c] > 0
+            t_map[c] -= 1
+
+            if need == 0:
+                while left < right and t_map[s[left]] < 0:
+                    t_map[s[left]] += 1
+                    left += 1
+                
+                if (right - left + 1) < min_len:
+                    min_len = (right - left + 1)
+                    min_context = [left, right]
+                
+                t_map[s[left]] += 1
+                left += 1
+                need += 1
         
-        context_map = {}
-        have = 0
-        need = len(t_map)
-        start = 0
-        res_len = float('inf')
-        res = [-1, -1]
-        for i in range(len(s)):
-            c = s[i]
-            context_map[c] = 1 + context_map.get(c, 0)
-
-            if c in t and context_map[c] == t_map[c]:
-                have += 1
-
-            while have == need:
-                if (i - start + 1) < res_len:
-                    res_len = i - start + 1
-                    res = [start, i]
-                context_map[s[start]] -= 1
-                if s[start] in t and context_map[s[start]] < t_map[s[start]]:
-                    have -= 1
-                start += 1
-
-        l, r = res[0], res[1]
-        return s[l:r+1] if res_len != float('inf') else ''
+        return s[min_context[0]:min_context[1]+1] if min_len != float('inf') else ''
